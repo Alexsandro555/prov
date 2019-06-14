a087f469724885e90acc53aac048bd24ff018f79
alexsandro <alexsandro@oooleader.ru>
2019-06-14 10:49:58 +0300

    update article title

--- Modules/Article/Resources/views/show.blade.php
+++ Modules/Article/Resources/views/show.blade.php
@@ -3,20 +3,17 @@
 @section('title', $article->title)
 
 @section('content')
-  <div class="content-wrapper  text-xs-left">
-   <!--<v-content>
-      <v-layout row wrap>-->
-        <div class="content text-xs-left">
-          <v-card class="py-5 ma-2 article-card" width="95%">
-            <p class="article__header text-md-left mx-5">
-              {{$article->title}}
-            </p>
-            <div class="article__content">
-              {!! $article->content !!}
-            </div>
-          </v-card>
+  <div class="content-wrapper text-xs-left">
+    <div class="content text-xs-left">
+      <v-card class="py-5 ma-2 article-card" width="95%">
+        <p class="article__header text-md-left mx-5 display-1">
+          {{$article->title}}
+        </p>
+        <div class="article__content">
+          {!! $article->content !!}
         </div>
-      <!--</v-layout>
-    </v-content>-->
+      </v-card>
+    </div>
   </div>
 @stop
+
