8046a1c21ddded382b4f7f4905ae19e428897b80
alexsandro <alexsandro@oooleader.ru>
2019-09-03 08:31:31 +0300

    change email

--- config/info.php
+++ config/info.php
@@ -5,6 +5,6 @@ return [
   'address' => 'г.Москва, ул. Большая Семеновская, дом 11, стр 11',
   'time_start' => '8-20',
   'time_end' => '17-20',
-  'email' => 'anna@oooleader.ru',
+  'email' => 'xanmaster08@rambler.ru',
   'admin_email' => 'alexsandro@oooleader.ru'
 ];
