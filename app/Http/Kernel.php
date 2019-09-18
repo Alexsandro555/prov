fcc95135ea756ce4a77d90d1d1f3135f15971a70
alexsandro <alexsandro@oooleader.ru>
2019-09-18 13:35:05 +0300

    add routing middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,6 +61,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'admin' => \Modules\Auth\Http\Middleware\Admin::class,
+        'visitor' => \Modules\Guest\Http\Middleware\GuestVisited::class
     ];
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -11,29 +11,21 @@
 |
 */
 
-Route::get('/', 'SiteController@index')->name('main');
 Route::get('/menu-left', 'SiteController@menuLeft');
 Route::get('/left-menu', 'SiteController@menuLeft');
-
-Route::get('/catalog/{slug}', ['uses'=>'SiteController@catalog', 'as'=>'catalog.product-category']);
-Route::get('/catalog/detail/{slug}',['uses' => 'SiteController@detail', 'as' => 'catalog.detail']);
-Route::get('/catalog/{slugProductCategory}/{slug}', ['uses' => 'SiteController@typeProduct', 'as'=>'catalog.type-product']);
-Route::get('/catalog/section-{slugSection}/{slugProductCategory}/{slug}', ['uses' => 'SiteController@section', 'as'=>'catalog.section']);
-Route::get('/catalog/{slugProductCategory}/{slugTypeProduct}/{slug}', ['uses' => 'SiteController@lineProduct', 'as'=>'catalog.line-product']);
-
-
 Route::get('/admin', ['uses' => '\Modules\Auth\Http\Controllers\AdminController@index', 'as' => 'master']);
 
-Route::get('/find/{text?}', ['uses' => 'FindController@index', 'as' => 'find']);
-Route::get('/sale', ['uses' => 'SiteController@sale', 'as' => 'sale']);
+Route::middleware(['visitor'])->group(function() {
+  Route::get('/', 'SiteController@index')->name('main');
+  Route::get('/catalog/{slug}', ['uses'=>'SiteController@catalog', 'as'=>'catalog.product-category']);
+  Route::get('/catalog/detail/{slug}',['uses' => 'SiteController@detail', 'as' => 'catalog.detail']);
+  Route::get('/catalog/{slugProductCategory}/{slug}', ['uses' => 'SiteController@typeProduct', 'as'=>'catalog.type-product']);
+  Route::get('/catalog/section-{slugSection}/{slugProductCategory}/{slug}', ['uses' => 'SiteController@section', 'as'=>'catalog.section']);
+  Route::get('/catalog/{slugProductCategory}/{slugTypeProduct}/{slug}', ['uses' => 'SiteController@lineProduct', 'as'=>'catalog.line-product']);
+  Route::get('/find/{text?}', ['uses' => 'FindController@index', 'as' => 'find']);
+  Route::get('/sale', ['uses' => 'SiteController@sale', 'as' => 'sale']);
+  Route::get('/{slug}', '\Modules\Page\Http\Controllers\PagesController@show');
+});
 
-//Route::get('/news/{slug}', '\Modules\News\Http\Controllers\NewsController@show');
-//Route::get('/{slug}', '\Modules\Page\Http\Controllers\PageController@show');
 
-//Auth::routes();
-//Route::post('/register', 'Auth\RegisterController@create');
-
-
-//Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/{slug}', '\Modules\Page\Http\Controllers\PagesController@show');
 
